36cd0f44fe79c8b13d14b3dfae6d244da4015571
Seth Zegelstein <seze4251@colorado.edu>
2016-02-07 11:14:42 -0700

    oh yeah its working

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Seths First Shell Script to give pretty graphs for my report
-counter=100
-while [ $counter -lt 1500];do
+counter="100"
+while [ $counter -lt 1500 ]
+do
 	./matrixmult $counter
 	((counter+=100))
 done
