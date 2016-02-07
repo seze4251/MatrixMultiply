65f963eee28134c5ffbf25169873aefdd813c88e
Seth Zegelstein <seze4251@colorado.edu>
2016-02-07 14:43:11 -0700

    check

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Seths First Shell Script to give pretty graphs for my report
-counter="100"
-while [ $counter -lt 1500 ]
+counter="400"
+while [ $counter -lt 3000 ]
 do
 	./matrixmult $counter
 	((counter+=100))
