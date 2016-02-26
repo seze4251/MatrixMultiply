c26c6244f1b66ac5e6da7c5273f1b217c0d748df
Seth Zegelstein <seze4251@colorado.edu>
2016-02-25 18:56:45 -0700

    Last Bug Hopefully!!!!

--- slaveMaster.c
+++ slaveMaster.c
@@ -160,14 +160,14 @@ int main(int argc, char * argv[]) {
                 handleSlaveBody(mtxA, mtxB, mtxC, serverRank, tagA, tagC, m, myrank, req, status); //Decide what to do with Request
                 
             } else if (status.MPI_TAG == tagFinilize) {
-                printf("Slave: %d Enetering Finish Manuever\n");
+                printf("Slave: %d Enetering Finish Manuever\n", myrank);
                 handleSlaveFinish(trash, serverRank, tagFinilize, myrank, req); // Decide what to do with this shit!
                 break;
             }
         }
     }
     
-    printf("I rank %d made it out of the infinite loop\n");
+    printf("I rank %d made it out of the infinite loop\n", myrank);
     if (myrank == serverRank) {
         handleServerFinish(mtxA, mtxB, mtxC, n, p, starttime, endtime, totaltime);
     }
